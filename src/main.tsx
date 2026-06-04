b6722522ef9180ec81547e118aa97b7f554e219b
Stoned_Academia <StonedAcademia@users.noreply.github.com>
2026-06-04 01:24:22 -0400

    Split global styles into focused modules

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "katex/dist/katex.min.css";
 import "./index.css";
+import "./lib/styles/motion-core.css";
+import "./lib/styles/motion-controls.css";
+import "./lib/styles/page-surfaces.css";
+import "./lib/styles/markdown.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
